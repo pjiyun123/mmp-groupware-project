25da2932e02a689468c006a76876ac55ce127f60
parkjiyun <pjyun05200@gmail.com>
2022-11-03 13:23:46 +0900

    build

---
+++ groupware-project/src/main/java/com/team4/groupwareproject/GroupwareProjectApplication.java
@@ -0,0 +1,13 @@
+package com.team4.groupwareproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupwareProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupwareProjectApplication.class, args);
+    }
+
+}
