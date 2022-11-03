6393f55c8722c54dab1468c0c8f4f3e7d9bd9666
parkjiyun <pjyun05200@gmail.com>
2022-11-03 13:12:14 +0900

    build

---
+++ groupware-project/src/test/java/com/team4/groupwareproject/GroupwareProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4.groupwareproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupwareProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
