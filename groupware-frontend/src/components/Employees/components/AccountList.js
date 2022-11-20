a63e50bb68b5129f53cbc9e709a8a0b3e5142b11
thumbzzero <thumbzzero@gmail.com>
2022-11-20 18:33:16 +0900

    Apply get users api

--- groupware-frontend/src/components/Employees/components/AccountList.js
+++ groupware-frontend/src/components/Employees/components/AccountList.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import PageNumbers from '../../Common/PageNumbers';
 import InquiryResult from './InquiryResult';
 
 const AccountList = () => {
+
+	const [list, setList] = useState([]);
+
+	useEffect(() => {
+		axios({
+			method: "get", 
+			url: "//localhost:8080/users", 
+		})
+		.then((response) => {
+			setList(response.data);
+		})
+	} , []);
 	return (
 		<div>
-			<InquiryResult />
+			<InquiryResult list={list} />
 			<PageNumbers />
 		</div>
 	);
--- groupware-frontend/src/components/Header/HeaderContainer.js
+++ groupware-frontend/src/components/Header/HeaderContainer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import classes from './styles/HeaderContainer.module.css'
 
 const HeaderContainer = () => {
 	return (
 		<div>
-			<h1>GROUPWARE - HEADER LOGO</h1>
+			<h1 className={classes.logo}>
+				SI<span className={classes.mmp}>MMP</span>LEWARE
+			</h1>
 		</div>
 	);
 };
