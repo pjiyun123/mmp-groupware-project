487ee06c4bb74098f4c66358a1e640ce99b9bb68
thumbzzero <thumbzzero@gmail.com>
2022-11-20 23:28:49 +0900

    Modify port

--- groupware-project/src/main/java/com/team4/groupwareproject/controller/UserController.java
+++ groupware-project/src/main/java/com/team4/groupwareproject/controller/UserController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3030")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     private final UserService userService;
