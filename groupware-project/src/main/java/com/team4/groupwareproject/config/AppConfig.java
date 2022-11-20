e294ab9dcaf684d44f52ebffe23b07383edc8747
parkjiyun <pjyun05200@gmail.com>
2022-11-20 23:25:33 +0900

    add AppConfig

---
+++ groupware-project/src/main/java/com/team4/groupwareproject/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.team4.groupwareproject.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan
+public class AppConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
