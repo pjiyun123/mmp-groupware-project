6978632baac611b5b07b39d11c4cb320e425450f
parkjiyun <pjyun05200@gmail.com>
2022-11-21 17:24:26 +0900

    add calendar update and delete functions

--- groupware-project/src/main/java/com/team4/groupwareproject/controller/CalendarController.java
+++ groupware-project/src/main/java/com/team4/groupwareproject/controller/CalendarController.java
@@ -6,6 +6,7 @@ import com.team4.groupwareproject.repository.CalendarRepository;
 import com.team4.groupwareproject.repository.UserRepository;
 import com.team4.groupwareproject.service.CalendarService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -57,6 +58,27 @@ public class CalendarController {
         calendarRepository.save(newCalendar);
 
         return newCalendar;
+    }
+
+    //일정 수정 페이지
+    @GetMapping("/calendar/updateForm/{calId}")
+    public String updateCalenderForm(@PathVariable Long calId, Model model) {
+        model.addAttribute("calendar", calendarService.getCalByCalId(calId));
+        return "/calendar/updateCalendarForm";
+    }
+
+    //calId로 일정 수정 처리
+    @PatchMapping("/calendar/{userId}/update/{calId}")
+    public Calendar updateCalendar(@PathVariable Long userId, @PathVariable Long calId, @RequestBody Calendar calendar) {
+        User user = userRepository.findByUserId(userId);
+        return calendarService.updateCalByCalId(user, calId, calendar);
+    }
+
+    //calId로 일정 삭제
+    @DeleteMapping("/calendar/delete/{calId}")
+    public List<Calendar> deleteCalendar(@PathVariable Long calId) {
+        calendarService.deleteCalByCalId(calId);
+        return calendarService.getCalendars();
     }
 
 }
--- groupware-project/src/main/java/com/team4/groupwareproject/service/CalendarService.java
+++ groupware-project/src/main/java/com/team4/groupwareproject/service/CalendarService.java
@@ -1,6 +1,7 @@
 package com.team4.groupwareproject.service;
 
 import com.team4.groupwareproject.domain.Calendar;
+import com.team4.groupwareproject.domain.User;
 import com.team4.groupwareproject.repository.CalendarRepository;
 import com.team4.groupwareproject.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +26,27 @@ public class CalendarService {
 
     public Calendar getCalByCalId(Long calId) {
         return calendarRepository.findByCalId(calId);
+    }
+
+    public Calendar updateCalByCalId(User user, Long calId, Calendar calendar) {
+        Calendar tempCal = calendarRepository.findByCalId(calId);
+
+        tempCal.setUser(user);
+        tempCal.setCalCategory(calendar.getCalCategory());
+        tempCal.setTitle(calendar.getTitle());
+        tempCal.setContent(calendar.getContent());
+        tempCal.setCalDate(calendar.getCalDate());
+        tempCal.setCalStartTime(calendar.getCalStartTime());
+        tempCal.setCalEndTime(calendar.getCalEndTime());
+        tempCal.setCalPlace(calendar.getCalPlace());
+        tempCal.setMajorYn(calendar.getMajorYn());
+
+        Calendar updatedCal = calendarRepository.save(tempCal);
+        return updatedCal;
+    }
+
+    public void deleteCalByCalId(Long calId) {
+        calendarRepository.deleteById(calId);
     }
 
 }
