f0c8e6a3e4a0843f4774214863e5162e6c34f329
thumbzzero <thumbzzero@gmail.com>
2022-11-22 00:18:14 +0900

    Change rank&authorization input form to dropdown

--- groupware-frontend/src/components/Employees/components/AccountCreate.js
+++ groupware-frontend/src/components/Employees/components/AccountCreate.js
@@ -3,6 +3,11 @@ import React, { useEffect, useRef, useState } from "react";
 import DropdownInput from "./DropdownInput";
 
 const AccountCreate = () => {
+  const [created, setCreated] = useState(false);
+  useEffect(() => {
+    setCreated(false);
+  }, []);
+
   const deptTypes = [
     {id: 0, value: '부서를 선택하세요.'},
     {id: 1, value: '설계팀'},
@@ -12,12 +17,34 @@ const AccountCreate = () => {
     {id: 5, value: '관리팀'},
   ];
 
+  const rankTypes = [
+    {id: 0, value: '직급을 선택하세요.'},
+    {id: 1, value: '사장'},
+    {id: 2, value: '부장'},
+    {id: 3, value: '차장'},
+    {id: 4, value: '과장'},
+    {id: 5, value: '대리'},
+    {id: 6, value: '주임'},
+    {id: 7, value: '사원'},
+  ];
+
+  const authorizationTypes = [
+    {id: 0, value: '부서를 선택하세요.'},
+    {id: 1, value: '관리자'},
+    {id: 2, value: '고급'},
+    {id: 3, value: '일반'},
+  ];
+
   const userNameInputRef = useRef();
   const [enteredDept, setEnteredDept] = useState(
     deptTypes[0].value
   );
-  const rankInputRef = useRef();
-  const authorizationInputRef = useRef();
+  const [enteredRank, setEnteredRank] = useState(
+    deptTypes[0].value
+  );
+  const [enteredAuthorization, setEnteredAuthorization] = useState(
+    deptTypes[0].value
+  );
   const phoneInputRef1 = useRef();
   const phoneInputRef2 = useRef();
   const phoneInputRef3 = useRef();
@@ -29,8 +56,6 @@ const AccountCreate = () => {
     event.preventDefault();
 
     const enterenUserName = userNameInputRef.current.value;
-    const enteredRank = rankInputRef.current.value;
-    const enteredAuthorization = authorizationInputRef.current.value;
     const enteredPhone1 = phoneInputRef1.current.value;
     const enteredPhone2 = phoneInputRef2.current.value;
     const enteredPhone3 = phoneInputRef3.current.value;
@@ -51,12 +76,15 @@ const AccountCreate = () => {
       phone: enteredPhone,
       email: enteredEmail,
     };
-    console.log(newEmployeeInfo);
+    
     axios({
       method: "post",
       url: "//localhost:8080/users/create",
       data: newEmployeeInfo,
-    }).then((response) => console.log(response));
+    }).then((response) => {
+      // console.log(response);
+      setCreated(true);
+    });
   };
 
   return (
@@ -73,10 +101,18 @@ const AccountCreate = () => {
           />
         </h3>
         <h3 className="infoType">
-          직급 : <input type="text" ref={rankInputRef} />
+          직급 :{" "}
+          <DropdownInput
+            dropdownList={rankTypes}
+            setSelectedDropValue={setEnteredRank}
+          />
         </h3>
         <h3 className="infoType">
-          권한 : <input type="text" ref={authorizationInputRef} />
+          권한 :{" "}
+          <DropdownInput
+            dropdownList={authorizationTypes}
+            setSelectedDropValue={setEnteredAuthorization}
+          />
         </h3>
         <h3 className="infoType">
           연락처 :{" "}
@@ -95,6 +131,7 @@ const AccountCreate = () => {
         </h3>
         <button className="accountCreateBtn">생성</button>
       </form>
+      {created ? <h4>계정이 생성되었습니다.</h4> : null}
     </div>
   );
 };
