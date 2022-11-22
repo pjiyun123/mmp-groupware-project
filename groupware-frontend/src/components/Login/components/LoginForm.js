481b440f86b7c9d6ef5c60ebb3a9464bb252f96f
thumbzzero <thumbzzero@gmail.com>
2022-11-22 21:03:53 +0900

    Add login functionality

--- groupware-frontend/src/components/Login/components/LoginForm.js
+++ groupware-frontend/src/components/Login/components/LoginForm.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import classes from "../styles/LoginForm.module.css";
 
-const LoginForm = () => {
+const LoginForm = ({ onSubmit, idRef, pwRef, message }) => {
+  // const idRef = useRef();
+  // const pwRef = useRef();
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={onSubmit} >
       <input
         className={classes.input}
         type="text"
         placeholder="아이디를 입력하세요."
+        ref={idRef}
       />
       <input
         className={classes.input}
         type="password"
         placeholder="비밀번호를 입력하세요."
+        ref={pwRef}
       />
+      <div>{message}</div>
       <button className={classes.button}>로그인</button>
     </form>
   );
--- groupware-frontend/src/pages/components/LoginPage.js
+++ groupware-frontend/src/pages/components/LoginPage.js
@@ -3,11 +3,11 @@ import LoginContainer from '../../components/Login/LoginContainer';
 import LogoContainer from '../../components/Login/LogoContainer';
 import '../../styles/loginPage.css';
 
-const LoginPage = () => {
+const LoginPage = ({ setIsLogin, setLoginedUser }) => {
 	return (
 		<div className='login-page'>
 			<LogoContainer />
-			<LoginContainer />
+			<LoginContainer setIsLogin={setIsLogin} setLoginedUser={setLoginedUser} />
 		</div>
 	);
 };
