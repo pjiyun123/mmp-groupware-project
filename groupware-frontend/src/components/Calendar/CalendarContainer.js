dbd802eef45c18ed2db1a5ac54394404461c9097
thumbzzero <thumbzzero@gmail.com>
2022-11-23 14:39:18 +0900

    Apply get calendar api

--- groupware-frontend/src/components/Calendar/CalendarContainer.js
+++ groupware-frontend/src/components/Calendar/CalendarContainer.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import CalCreate from './components/CalCreate';
 import Calendar from './components/Calendar';
+import axios from 'axios';
 
 const CalendarContainer = () => {
+	const [calList, setCalList] = useState([]);
+
+	useEffect(() => {
+    axios({
+      method: "get",
+      url: "//localhost:8080/calendar",
+    }).then((response) => {
+      setCalList(response.data);
+    })
+  }, []);
+
 	return (
 		<div className='calendarContainer'>
 			<Routes>
-				<Route path='/' element={<Calendar />} />
+				<Route path='/' element={<Calendar calList={calList} />} />
 				<Route path="create" element={<CalCreate />} />
 			</Routes>
 		</div>
