b84128fa66bca526249fb17b52b7d5d7eaee0347
parkjiyun <pjyun05200@gmail.com>
2022-11-28 13:45:59 +0900

    add approval service and repo

---
+++ groupware-project/src/main/java/com/team4/groupwareproject/domain/constant/apprStatus.java
@@ -0,0 +1,12 @@
+package com.team4.groupwareproject.domain.constant;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class apprStatus {
+
+    public final static Long apprWait = 1L;
+    public final static Long apprYes= 2L;
+    public final static Long apprNo = 3L;
+
+}
