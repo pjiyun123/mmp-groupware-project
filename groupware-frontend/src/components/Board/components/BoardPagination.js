96cb297065c425d6a840390f4dd493e1cf6d8bd1
thumbzzero <thumbzzero@gmail.com>
2022-12-05 02:55:42 +0900

    Apply delete businesslog api

--- groupware-frontend/src/components/Board/components/BoardPagination.js
+++ groupware-frontend/src/components/Board/components/BoardPagination.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import classes from '../styles/BoardPagination.module.css';
 
 const BoardPagination = ({ menuType, data }) => {
